8eb6de0e39b32995a64e6267d576844ddee1cae0
Shivam Arora <97466421+Arora-Shivam@users.noreply.github.com>
2022-09-03 16:43:23 +0530

    Time: 0 ms (100.00%), Space: 40.9 MB (50.41%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.java
+++ 70-climbing-stairs/70-climbing-stairs.java
@@ -10,21 +10,29 @@ class Solution {
     public int climbStairs(int n,int[] dp) {
         
         
-        if(n<0){
-            return 0;
-        }
+//         if(n<0){
+//             return 0;
+//         }
         
-        if(n==0){
-            return 1;
-        }
+//         if(n==0){
+//             return 1;
+//         }
         
-        if(dp[n]!=-1){
-            return dp[n];
-        }
-        else{
-            dp[n] = climbStairs(n-1,dp)+climbStairs(n-2,dp);
-        }
+//         if(dp[n]!=-1){
+//             return dp[n];
+//         }
+//         else{
+//             dp[n] = climbStairs(n-1,dp)+climbStairs(n-2,dp);
+//         }
         
-        return dp[n];
+//         return dp[n];
+        
+        dp[0]=1;
+        dp[1]=1;
+        
+        for(int i=2;i<dp.length;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[dp.length-1];
     }
 }
