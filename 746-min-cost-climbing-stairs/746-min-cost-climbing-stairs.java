148fa8b5b42dd98f0a5c0896c530dc9f53345518
Shivam Arora <97466421+Arora-Shivam@users.noreply.github.com>
2022-09-03 18:04:35 +0530

    Time: 1 ms (89.91%), Space: 43.9 MB (33.34%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.java
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.java
@@ -14,22 +14,8 @@ class Solution {
          return Math.min(a,b);
         
         
-       // return minCostClimbingStairs(cost);
+      
     }
     
-   //  public int minCostClimbingStairs(int[] cost,int[] dp) {
-        
-       
-//          dp[0]=cost[0];
-//          dp[1]=cost[1];
-         
-//          for(int i=2;i<dp.length;i++){
-//              dp[i]=Math.min(cost[i]+dp[i-1],cost[i]+dp[i-2]);
-             
-//          }
-         
-//          return Math.min(dp[dp.length-1],dp[dp.length-2]);
-         
-        
-   // }
+  
 }
