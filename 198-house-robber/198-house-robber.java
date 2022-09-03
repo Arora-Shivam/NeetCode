378a8f1e4cf478268dfa7b94baf695da18e4fce4
Shivam Arora <97466421+Arora-Shivam@users.noreply.github.com>
2022-09-04 00:22:57 +0530

    Time: 0 ms (100.00%), Space: 39.5 MB (98.33%) - LeetHub

--- 198-house-robber/198-house-robber.java
+++ 198-house-robber/198-house-robber.java
@@ -1,21 +1,42 @@
 class Solution {
     public int rob(int[] nums) {
-        int[] dp= new int[nums.length];
-        if(dp.length==1){
+//         int[] dp= new int[nums.length];
+//         if(dp.length==1){
+//             return nums[0];
+//         }
+//         else{
+            
+            
+//             dp[1]=Math.max(nums[0],nums[1]);
+//             dp[0]=nums[0];
+            
+//             for(int i=2;i<dp.length;i++){
+
+//                 dp[i]=Math.max(dp[i-1],dp[i-2]+nums[i]);
+//             }
+
+//             return Math.max(dp[dp.length-1],dp[dp.length-2]);
+//         }
+        
+                
+        if(nums.length==1){
             return nums[0];
         }
         else{
             
+            int a=nums[0];
+            int b=Math.max(nums[0],nums[1]);
             
-            dp[1]=Math.max(nums[0],nums[1]);
-            dp[0]=nums[0];
-            
-            for(int i=2;i<dp.length;i++){
+            int c=0;
+            for(int i=2;i<nums.length;i++){
 
-                dp[i]=Math.max(dp[i-1],dp[i-2]+nums[i]);
+                c=Math.max(b,a+nums[i]);
+                
+                a=b;
+                b=c;
             }
 
-            return Math.max(dp[dp.length-1],dp[dp.length-2]);
+            return Math.max(b,a);
         }
     }
 }
