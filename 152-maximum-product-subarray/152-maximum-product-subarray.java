d04ffcd9e0646744b2a89b298675de18e73bc4d0
Shivam Arora <97466421+Arora-Shivam@users.noreply.github.com>
2022-09-14 01:06:38 +0530

    Time: 4 ms (23.00%), Space: 45.1 MB (36.01%) - LeetHub

--- 152-maximum-product-subarray/152-maximum-product-subarray.java
+++ 152-maximum-product-subarray/152-maximum-product-subarray.java
@@ -2,28 +2,33 @@ class Solution {
     
     public int maxProduct(int[] nums) {
         
-        int[][] dp=new int[nums.length][2];  
-        dp[0][0]=nums[0];
-        dp[0][1]=nums[0];
+        // int[][] dp=new int[nums.length][2];  
+        // dp[0][0]=nums[0];
+        // dp[0][1]=nums[0];
+        int currMax=nums[0];
+        int currMin=nums[0];
+        int PrevMin=nums[0];
+        int PrevMax=nums[0];
         int max=nums[0];
         
         for(int i=1;i<nums.length;i++){
             
             if(nums[i]==0){
-                dp[i][0]=1;
-                dp[i][1]=1;
+                currMax=1;
+                currMin=1;
                 max=Math.max(max,0);
             }
             else{
-                dp[i][0]=Math.max(dp[i-1][0]*nums[i],Math.max(dp[i-1][1]*nums[i],nums[i]));
-                dp[i][1]=Math.min(dp[i-1][0]*nums[i],Math.min(dp[i-1][1]*nums[i],nums[i]));
-                max=Math.max(max,Math.max(dp[i][0],dp[i][1]));
+                currMax=Math.max(PrevMax*nums[i],Math.max(PrevMin*nums[i],nums[i]));
+                currMin=Math.min(PrevMax*nums[i],Math.min(PrevMin*nums[i],nums[i]));
+                max=Math.max(max,Math.max(currMax,currMin));
             }
-            
+            PrevMin=currMin;
+            PrevMax=currMax;
         }
-        for(int[] d : dp){
-            System.out.println(Arrays.toString(d));
-        }     
+        // for(int[] d : dp){
+        //     System.out.println(Arrays.toString(d));
+        // }     
         
         return max;
     }
